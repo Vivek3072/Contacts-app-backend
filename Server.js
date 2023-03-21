43bc9a55328023b8006a387636f91f21b258bf94
Vivek Srivastava <88632897+Vivek3072@users.noreply.github.com>
2023-03-22 01:59:32 +0530

    USer authentication

--- Server.js
+++ Server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv").config();
 
 const ContactRoutes = require("./routes/ContactRoutes");
+const UserRoutes = require("./routes/UserRoutes");
 const errorHandler = require("./middleware/ErrorHandler");
 const connectDb = require("./config/DBConnection");
 
@@ -15,6 +16,7 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 
 app.use("/api/contacts", ContactRoutes);
+app.use("/api/users", UserRoutes);
 
 app.use(errorHandler);
 
